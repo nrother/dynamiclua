329bdf01cf05dab7faeb5471ebfca44ce230f9c5
Niklas Rother <nrother@dataline.de>
2014-02-28 12:55:31 +0100

    Removed NLua dependency, included NLua locally. The NLua NuGet package is currently broken :(

--- DynamicLua/Properties/AssemblyInfo.cs
+++ DynamicLua/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Niklas Rother")]
 [assembly: AssemblyProduct("DynamicLua")]
-[assembly: AssemblyCopyright("Copyright © Niklas Rother 2011-2013")]
+[assembly: AssemblyCopyright("Copyright © Niklas Rother 2011-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
